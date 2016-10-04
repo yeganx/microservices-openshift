531b94c065318a4adc728810c31a4f507c5ce5a8
David Sancho <dsanchor@redhat.com>
2016-10-04 17:37:31 +0200

    Added a sample unit test (empty) and jacoco plugin to generate test coverage in sonar reports

---
+++ trip-validation-service/web/src/test/java/com/redhat/openshift/hackfest/test/SampleTest.java
@@ -0,0 +1,14 @@
+package com.redhat.openshift.hackfest.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
